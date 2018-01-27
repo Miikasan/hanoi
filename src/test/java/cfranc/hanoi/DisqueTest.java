b506cfdb2f27b979afac249246c8b8de8525b660
Miikasan <mikail.alptekin@iut-dijon.u-bourgogne.fr>
2018-01-27 18:44:07 +0100

    Add files via upload

--- src/test/java/cfranc/hanoi/DisqueTest.java
+++ src/test/java/cfranc/hanoi/DisqueTest.java
@@ -1,19 +1,26 @@
 package cfranc.hanoi;
 
+import org.junit.After;
+import org.junit.AfterClass;
 import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.BeforeClass;
 
 import org.junit.Test;
 
 public class DisqueTest {
 
-	@Test
-	public void testCompareTo() {
-		Disque petit = new Disque(1);
-		Disque moyen = new Disque(2);
-		Disque grand = new Disque(3);
-		assertEquals(-1, petit.compareTo(moyen));
-		assertEquals(-1, moyen.compareTo(grand));
-		//TODO : à compléter
-	}
-
+    @Test
+    public void testCompareTo() {
+            Disque petit = new Disque(1);
+            Disque petit2 = new Disque(1);
+            Disque moyen = new Disque(2);
+            Disque grand = new Disque(3);
+            
+            assertEquals(0, petit.compareTo(petit2));
+            assertEquals(-1, petit.compareTo(moyen));
+            assertEquals(-1, moyen.compareTo(grand));
+            assertEquals(1, grand.compareTo(moyen));
+            assertEquals(1, moyen.compareTo(petit));
+    }
 }
